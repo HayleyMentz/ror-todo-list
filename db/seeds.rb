12f4a18599ce28b01343a98fce58b2a1d25f4ed5
Hayley Mentz <hayley.mentz@gmail.com>
2022-06-26 17:10:31 -0700

    list back end done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+#  doing it by hand
+#List.create(title: 'Grocery', desc:'Food for the week')
+#List.create(title: 'Office', desc:'Office Supplie')
+#List.create(title: 'Bunker', desc:'Meal Prep')
+#List.create(title: 'Gym', desc:'Daily workout')
+
+
+#Delete any list that was already there mainly for tests
+List.delete_all
+
+@counter = 0
+
+#create it 10 times
+10.times do
+List.create(
+  title: "#{Faker::Hipster.word}#{@counter}",
+  desc: Faker::Hipster.sentence
+)
+@counter += 1
+end
+
+# to run => bundle exec rails db:seed
